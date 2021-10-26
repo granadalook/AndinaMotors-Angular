98365021a394eca179745c4cb466cd530baabb84
granadalook <Tiven17.jsga@gmail.com>
2021-10-26 18:47:06 -0500

    feature/desarrillo_Angular: se modulariza home  y se crean  2 componentes

---
+++ src/app/components/home/components/aceites/aceites.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AceitesComponent } from './aceites.component';
+
+describe('AceitesComponent', () => {
+  let component: AceitesComponent;
+  let fixture: ComponentFixture<AceitesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AceitesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AceitesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { homeRoutingModule } from './home-routing.module';
+import { AceitesComponent } from './components/aceites/aceites.component';
+import { ClasificacionComponent } from './components/clasificacion/clasificacion.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AceitesComponent, ClasificacionComponent],
   imports: [homeRoutingModule],
 })
 export class HomeModule {}
